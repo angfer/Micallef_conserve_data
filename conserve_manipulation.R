17e9b4a03d85899373b17988ae198a990ef9ba5c
Angela Ferelli <Angela Ferelli>
2017-08-02 16:51:37 -0400

    end of day 08-2-2017 mapped conserve indicator box plots across time and box plots for spnono rep4

--- conserve_manipulation.R
+++ conserve_manipulation.R
@@ -9,7 +9,7 @@ indicators2 <-indicators[-98, ]
 str(indicators2)
 View(indicators2)
 
-spnono_4 <- read.csv('../data_r/07_25_17_spnono_serovars.csv')
+spnono_4 <- read.csv('../07_25_17_spnono_serovars.csv',na.strings= c("","na"))
 View(spnono_4)
 #i dont know why two extra columns were entered in spnono-4, so I deleted them 
 spnono_4 <- select(spnono_4, -X, -X.1)
@@ -25,5 +25,44 @@ xlab="Site type", ylab="CFU/mL")
 boxplot(E._coli~Site,data=indicators2, main="E. coli counts by site across dates",
         xlab="Site type", ylab="CFU/mL")
 
+library(dplyr)
+library(ggplot2)
+ggplot(data=indicators2, aes(x=Site, y=Total_coliform))+
+geom_boxplot() +geom_point(stat='summary',
+                                 fun.y='mean',
+                                  color='cyan')
+
+ggplot(data=indicators2, aes(x=Site, y=E._coli))+
+  geom_boxplot() +geom_point(stat='summary',
+                             fun.y='mean',
+                             color='red')
+
+ggplot(data=indicators2, aes(x=Site, y=Enterococcus))+
+  geom_boxplot() +geom_point(stat='summary',
+                             fun.y='mean',
+                             color='green')
+
+ggplot(data=indicators2, aes(x=Site, y=Aeromonas))+
+  geom_boxplot() +geom_point(stat='summary',
+                             fun.y='mean',
+                             color='yellow')
 
 
+
+indicators_MD10 <- filter(indicators2,
+                     Site=='MD10')
+ggplot(data = indicators2,
+       aes(x = Site, y = E._coli, color=Date))+
+geom_boxplot() +geom_point(stat='summary',
+                           fun.y='mean',
+                           color='cyan')
+
+ggplot(data = spnono_4,
+       aes(x = time, y = log_decline, color=Serovar))+
+  geom_boxplot() +geom_point(stat='summary',
+                             fun.y='mean',
+                             color='cyan')
+ggplot(data = spnono_4,
+       aes(x = time, y = log_decline, color=Serovar))+
+  geom_boxplot()+
+  facet_wrap(~NO_challenge)
